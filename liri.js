afbe7f905705549c68c200dda1b36bd140e9bb20
Derek Johnson <derekjohnson@Dereks-iMac-Pro.attlocal.net>
2018-02-03 00:25:03 -0500

    new stuff

--- liri.js
+++ liri.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 var fs = require("fs");
 var request = require ("request");
-require("dotenv").config();
+
 
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log(spotify);
+
 
 var action = process.argv[2];
 var nodeArgs = process.argv;
 var userInput = "";
-for (var i = 2; i < nodeArgs.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        console.log(nodeArgs);
         userInput = userInput + "+" + nodeArgs[i];
     }
     else {
@@ -23,7 +26,11 @@ for (var i = 2; i < nodeArgs.length; i++) {
 
 };
 
-console.log(userInput);
+
+// client.get('search/tweets', {q: 'star trek'}, function(error, tweets, response) {
+//     var statuses = tweets.statuses.map(function(tweet){return tweet.text});
+//     console.log(statuses);
+// });
 
 
 
@@ -43,14 +50,40 @@ switch (action) {
 }
 
 
-function omdbGrab(userInput) {
+function omdbGrab() {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl);
     request(queryUrl, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("Release Year: " + JSON.parse(body).Year);
+            var data = JSON.parse(body);
+            var ratingText = '';
+            var ratings = data.Ratings.filter(function(rating) {
+                var notMetacritic = rating.Source !== "Metacritic";
+                if (notMetacritic) {ratingText += rating.Source + ': ' + rating.Value + "\n"}
+                return notMetacritic;
+            });
+
+
+            console.log("Title: " + data.Title);
+            console.log("Release Year: " + data.Year);
+            console.log("IMDB Rating: " + data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + data.imdbRating);
+            console.log("Country: " + data.Country);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+            console.log("Actors: " + data.Actors);
+            console.log(ratingText);
         }
     });
 
 }
+
+//* Title of the movie.
+//* Year the movie came out.
+//* IMDB Rating of the movie.
+//* Rotten Tomatoes Rating of the movie.
+//* Country where the movie was produced.
+//* Language of the movie.
+//* Plot of the movie.
+//* Actors in the movie.
