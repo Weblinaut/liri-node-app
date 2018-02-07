3a6845cd8ade55f30d48aebebfc6bc4ad73723e9
Derek Johnson <derekjohnson@Dereks-iMac-Pro.attlocal.net>
2018-02-07 10:37:43 -0500

    added inquirer

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var inquirer = require('inquirer');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var fs = require("fs");
@@ -11,75 +12,69 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
+inquirer.prompt([
+    {
+        type: "list",
+        name: "userChoice",
+        message: "I am Liri, what would you like for me to do??",
+        choices: ["Search Twitter", "Get movie info", "Get song info", "Something Else..."]
+    },
+    {
+        type: "input",
+        name: "searchQuery",
+        message: "What would you like to search for???"
+    },
 
-var action = process.argv[2];
-var nodeArgs = process.argv;
-var userInput = "";
-for (var i = 3; i < nodeArgs.length; i++) {
-    if (i > 3 && i < nodeArgs.length) {
-        console.log(nodeArgs);
-        userInput = userInput + "+" + nodeArgs[i];
+]).then(function(response) {
+    var response = response;
+    if (response.userChoice === "Get movie info") {
+        var queryUrl = "http://www.omdbapi.com/?t=" + response.searchQuery + "&y=&plot=short&apikey=trilogy";
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var data = JSON.parse(body);
+                var ratingText = '';
+                var ratings = data.Ratings.filter(function (rating) {
+                    var notMetacritic = rating.Source !== "Metacritic";
+                    if (notMetacritic) {
+                        ratingText += rating.Source + ': ' + rating.Value + "\n"
+                    }
+                    return notMetacritic;
+                });
+
+                console.log("Title: " + data.Title + "\nRelease Year: " + data.Year + ratingText + "\nCountry: " + data.Country +"\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors);
+
+            }
+        });
     }
-    else {
-        userInput += nodeArgs[i];
+    if (response.userChoice === "Search Twitter") {
+        client.get('search/tweets', {q: response.searchQuery}, function(error, tweets, response) {
+            var statuses = tweets.statuses.map(function(tweet){return tweet.text});
+            console.log(statuses);
+
+        });
+
     }
-
-};
-
-
-// client.get('search/tweets', {q: 'star trek'}, function(error, tweets, response) {
-//     var statuses = tweets.statuses.map(function(tweet){return tweet.text});
-//     console.log(statuses);
-// });
+});
 
 
 
-switch (action) {
-    case "my-tweets":
-        twitGrab();
-        break;
-    case "spotify-this-song":
-        spotGrab();
-        break;
-    case "movie-this":
-        omdbGrab();
-        break;
-    case "do-what-it-says":
-        doWhat();
-        break;
-}
+// var action = process.argv[2];
+// var nodeArgs = process.argv;
+// var userInput = "";
+// for (var i = 3; i < nodeArgs.length; i++) {
+//     if (i > 3 && i < nodeArgs.length) {
+//         console.log(nodeArgs);
+//         userInput = userInput + "+" + nodeArgs[i];
+//     }
+//     else {
+//         userInput += nodeArgs[i];
+//     }
+//
+// };
 
 
-function omdbGrab() {
-
-    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
-    console.log(queryUrl);
-    request(queryUrl, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var data = JSON.parse(body);
-            var ratingText = '';
-            var ratings = data.Ratings.filter(function(rating) {
-                var notMetacritic = rating.Source !== "Metacritic";
-                if (notMetacritic) {ratingText += rating.Source + ': ' + rating.Value + "\n"}
-                return notMetacritic;
-            });
 
 
-            console.log("Title: " + data.Title);
-            console.log("Release Year: " + data.Year);
-            console.log("IMDB Rating: " + data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + data.imdbRating);
-            console.log("Country: " + data.Country);
-            console.log("Language: " + data.Language);
-            console.log("Plot: " + data.Plot);
-            console.log("Actors: " + data.Actors);
-            console.log(ratingText);
-        }
-    });
-
-}
-
-//* Title of the movie.
 //* Year the movie came out.
 //* IMDB Rating of the movie.
 //* Rotten Tomatoes Rating of the movie.
